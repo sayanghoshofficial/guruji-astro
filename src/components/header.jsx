c9e77911b947445b2a6dc4affe3857f6897fa159
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-04-16 10:47:23 +0530

    header completion

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import Navbar from './navbar';
+import React from "react";
+import Navbar from "./navbar";
+import Style from "../Styles/header.module.css";
 
-const Header =()=> {
+const Header = () => {
   return (
-    <div>
-      <Navbar/>
+    <div className={Style.mainHeader}>
+      <Navbar />
+      <div className={Style.header}>
+        <h1>"Astrology for Clarity"</h1>
+        <p>
+          Your Name is a Vedic Astrologer and has expertise in Vaastu and Mantra
+          Theraphy{" "}
+        </p>
+        <br />
+        <button>Contact Now</button>
+      </div>
+      <div className={Style.headerFooter}>
+        <div className={Style.iconBox}>
+          <i class="fa-solid fa-headset"></i> <p>24x7 Cutomer Support</p>
+        </div>
+        <div className={Style.iconBox}>
+          <i class="fa-solid fa-wallet"></i> <p>100% Refund if Unsatisfied</p>
+        </div>
+        <div className={Style.iconBox}>
+          <i class="fa-solid fa-lock"></i> <p>Private & Confidential</p>
+        </div>
+        <div className={Style.iconBox}>
+          <img
+            src="https://user-images.githubusercontent.com/99132893/232265600-5a03d185-1c89-4dd4-9d02-491565d80add.png"
+            alt="verified"
+          />{" "}
+          <p>Verified Astrologer</p>
+        </div>
+        <div className={Style.iconBox}>
+          <img
+            src="https://user-images.githubusercontent.com/99132893/232265922-d99b86ce-4eb8-4277-9ea3-4de68c7783ae.png"
+            alt="verified"
+          />{" "}
+          <p>Secure Payment</p>
+        </div>
+      </div>
+      <br />
+      <div className={Style.pageIndicator}>
+        <div className={Style.div1}></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Style from '../Styles/navbar.module.css';
+import Style from "../Styles/navbar.module.css";
 
 const Navbar = () => {
   return (
@@ -9,14 +9,19 @@ const Navbar = () => {
           src="https://media.glassdoor.com/sqll/4755382/guruji-astro-squarelogo-1645176134551.png"
           alt="web-logo"
         />
-       <span>Guruji</span> 
+        <span>Guruji</span>
       </div>
       <div className={Style.menu}>
         <p>Home</p>
         <p>Call with Astrologer</p>
         <p>live(Comming Soon)</p>
       </div>
-      <div className={Style.user}><img src=""/></div>
+      <div className={Style.user}>
+        <img
+          src="https://avatars.githubusercontent.com/u/99132893?s=400&u=e3458f17919693e71c9ad3530dfc732edb092213&v=4"
+          alt="user-profile-pic"
+        />
+      </div>
     </div>
   );
 };
